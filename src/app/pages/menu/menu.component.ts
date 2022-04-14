1ae030feb50041139229dd11e1865585f5856149
saikiran3115 <saikiran.gosangi@gmail.com>
2022-04-14 10:04:42 +0530

    initial commit

---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private service:OrderDetailsService) { }
+  carData:any;
+  ngOnInit(): void {
+    this.carData = this.service.carDetails;
+  }
+
+}
